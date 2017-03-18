dacb535e7b8a708818ce700990902e3be208c38d
Saul Rosales <saul.rosalesb@gmail.com>
2017-03-18 19:11:39 +0000

    Create rails application and setup home and about pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+
+  def home
+    @mainTitle = "Welcome to AlphaBlog Application"
+    @mainDesc = "It would be ab amazing blog application"
+  end
+  
+  def about
+    
+  end
+  
+end
