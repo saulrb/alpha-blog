a7ab11fde9b270ff9d335bce7b48f8f3798d320c
Saul Rosales <saul.rosalesb@gmail.com>
2017-03-24 03:57:48 +0000

    Enhanced home page with background landscape image

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def home
-    @mainTitle = "Welcome to AlphaBlog Application"
+    @mainTitle = "Welcome to AlphaBlog"
     @mainDesc = "It would be ab amazing blog application"
   end
   
