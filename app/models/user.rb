b1a494e75254d0b4ba97d464ecc17a01f2ecb743
Saul Rosales <saul.rosalesb@gmail.com>
2017-05-05 03:06:05 +0000

    Added association between users and articles and debugger

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :articles
+  before_save { self.email = email.downcase }
   validates :user_name, presence: true,
             uniqueness: { case_sensitive:false }, 
             length: { minimum: 3, maximum: 25 }
